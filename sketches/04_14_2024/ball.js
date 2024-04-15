277edd367869ec51eb19ffab675378735708f947
Chris Lawrie <clawrie2020@gmail.com>
2024-04-14 22:14:02 -0400

    get ball import working

--- sketches/04_14_2024/ball.js
+++ sketches/04_14_2024/ball.js
@@ -1,32 +1,33 @@
 class Ball {
-  constructor(x, y, radius) {
+  constructor(x, y, radius, width, height) {
     this.radius = radius;
     this.position_x = x;
     this.position_y = y;
-    this.velocityX = random(-5, 5); // Initial random velocity
-    this.velocityY = random(-5, 5); // Initial random velocity
+    this.velocity_x = random(-5, 5);
+    this.velocity_y = random(-5, 5);
+    this.width = width;
+    this.height = height;
   }
 
   update() {
-    this.position_x += this.velocityX;
-    this.position_y += this.velocityY;
-    this.velocityY += 0.1;
-
+    this.position_x += this.velocity_x;
+    this.position_y += this.velocity_y;
+    this.velocity_y += 0.2;
     if (
-      this.position_x + this.radius >= width ||
+      this.position_x + this.radius >= this.width ||
       this.position_x - this.radius <= 0
     ) {
-      this.velocityX *= -1;
+      this.velocity_x *= -0.95;
     }
-    if (
-      this.position_y + this.radius >= height ||
-      this.position_x - this.radius <= 0
-    ) {
-      this.velocityY *= -1;
+    if (this.position_y + this.radius >= this.height) {
+      this.velocity_y *= -0.95;
     }
   }
 
   display() {
-    ellipse(this.x, this.y, this.radius * 2);
+    fill(0);
+    stroke(0);
+    strokeWeight(5);
+    ellipse(this.position_x, this.position_y, this.radius * 2);
   }
 }
--- sketches/04_14_2024/sketch.js
+++ sketches/04_14_2024/sketch.js
@@ -1,55 +1,21 @@
 // Chris Lawrie | April 14th 2024
 // A ball bouncing around a box with gravity
 
-// import Ball from "ball.js";
-
-class Ball {
-  constructor(x, y, radius) {
-    this.radius = radius;
-    this.position_x = x;
-    this.position_y = y;
-    this.velocityX = random(-5, 5); // Initial random velocity
-    this.velocityY = random(-5, 5); // Initial random velocity
-  }
-
-  update() {
-    this.position_x += this.velocityX;
-    this.position_y += this.velocityY;
-    this.velocityY += 0.2;
-
-    if (
-      this.position_x + this.radius >= width ||
-      this.position_x - this.radius <= 0
-    ) {
-      this.velocityX *= -0.95;
-    }
-    if (
-      this.position_y + this.radius >=
-      height // ||
-      //   this.position_y - this.radius <= 0
-    ) {
-      this.velocityY *= -0.95;
-    }
-  }
-
-  display() {
-    fill(0);
-    ellipse(this.position_x, this.position_y, this.radius * 2);
-  }
-}
-
-let ball;
+var ball;
 
 function setup() {
   createCanvas(400, 400);
-  ball = new Ball(width / 2, 60, 20);
+  ball = new Ball(width / 2, 60, 20, width, height);
 }
 
 function draw() {
-  background(100);
+  background(255);
+
+  fill(255);
+  stroke(200);
+  strokeWeight(5);
+  square(0, 0, width, 20);
+
   ball.update();
   ball.display();
-
-  fill(0);
-  ellipse(this.x, this.y, this.radius * 2);
 }
