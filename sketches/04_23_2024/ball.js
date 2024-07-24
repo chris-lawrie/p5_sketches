fcd13a22e7884cd3e978b6ce20edb0e8e4aba88c
Chris Lawrie <clawrie2020@gmail.com>
2024-07-23 20:30:21 -0400

    add bool for collisions

--- sketches/04_23_2024/ball.js
+++ sketches/04_23_2024/ball.js
@@ -1,6 +1,7 @@
 var GRAVITY = 0.2;
-var NOTES = [261.63, 329.63, 392.0]; // Frequencies for C4, E4, and G4
-
+var NOTES = [
+  261.63, 293.66, 329.63, 349.23, 392.0, 440.0, 493.88, 523.25, 587.33, 659.25,
+];
 function angle(point_1, point_2) {
   var dx = point_1.x - point_2.x;
   var dy = point_1.y - point_2.y;
@@ -28,7 +29,7 @@ class Ball {
   }
 
   update() {
-    this.velocity.y += GRAVITY;
+    // this.velocity.y += GRAVITY;
     this.position.add(this.velocity);
 
     let dist_to_center = this.position.dist(this.circle_center);
--- sketches/04_23_2024/sketch.js
+++ sketches/04_23_2024/sketch.js
@@ -2,6 +2,7 @@
 // Balls bounce off eachother upon colliding
 
 let balls = [];
+let collisions = true;
 
 function random_color() {
   return color(random(150, 255), random(150, 255), random(150, 255), 200);
@@ -9,7 +10,7 @@ function random_color() {
 
 function setup() {
   createCanvas(400, 400);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     let x = random(100, 300);
     let y = random(100, 200);
     let radius = random(10, 10);
@@ -28,48 +29,54 @@ function draw() {
   strokeWeight(5);
   ellipse(width / 2, height / 2, 380);
 
-  // Check for collisions
+  // Update balls...
   for (let i = 0; i < balls.length; i++) {
     balls[i].update();
     balls[i].display();
 
-    // don't check against self & don't check the same comparison twice
-    // for (let j = i + 1; j < balls.length; j++) {
-    //   distance = dist(
-    //     balls[i].position.x,
-    //     balls[i].position.y,
-    //     balls[j].position.x,
-    //     balls[j].position.y
-    //   );
+    if (collisions) {
+      // don't check against self & don't check the same comparison twice
+      for (let j = i + 1; j < balls.length; j++) {
+        distance = dist(
+          balls[i].position.x,
+          balls[i].position.y,
+          balls[j].position.x,
+          balls[j].position.y
+        );
 
-    //   if (distance < balls[i].ball_radius + balls[j].ball_radius) {
-    //     // correct ball positions incase radii overlap
-    //     let overlap = distance - (balls[i].ball_radius + balls[j].ball_radius);
-    //     let impact_vector = p5.Vector.sub(balls[j].position, balls[i].position);
-    //     impact_vector.setMag(distance);
-    //     let dir = impact_vector.copy();
-    //     dir.setMag(overlap * 0.5);
-    //     balls[i].position.add(dir);
-    //     balls[j].position.sub(dir);
+        if (distance < balls[i].ball_radius + balls[j].ball_radius) {
+          // correct ball positions incase radii overlap
+          let overlap =
+            distance - (balls[i].ball_radius + balls[j].ball_radius);
+          let impact_vector = p5.Vector.sub(
+            balls[j].position,
+            balls[i].position
+          );
+          impact_vector.setMag(distance);
+          let dir = impact_vector.copy();
+          dir.setMag(overlap * 0.5);
+          balls[i].position.add(dir);
+          balls[j].position.sub(dir);
 
-    //     // calculate new velocities assuming elastic collisions
-    //     let tot_mass = balls[i].mass + balls[j].mass;
-    //     let velocity_diff = p5.Vector.sub(
-    //       balls[j].velocity,
-    //       balls[i].veclocity
-    //     );
+          // calculate new velocities assuming elastic collisions
+          let tot_mass = balls[i].mass + balls[j].mass;
+          let velocity_diff = p5.Vector.sub(
+            balls[j].velocity,
+            balls[i].veclocity
+          );
 
-    //     let numerator = velocity_diff.dot(impact_vector);
-    //     let denomenator = tot_mass * distance * distance;
+          let numerator = velocity_diff.dot(impact_vector);
+          let denomenator = tot_mass * distance * distance;
 
-    //     let delta_velocity_i = impact_vector.copy();
-    //     delta_velocity_i.mult((2 * balls[j].mass * numerator) / denomenator);
-    //     balls[i].velocity.add(delta_velocity_i);
+          let delta_velocity_i = impact_vector.copy();
+          delta_velocity_i.mult((2 * balls[j].mass * numerator) / denomenator);
+          balls[i].velocity.add(delta_velocity_i);
 
-    //     let delta_velocity_j = impact_vector.copy();
-    //     delta_velocity_j.mult((-2 * balls[i].mass * numerator) / denomenator);
-    //     balls[j].velocity.add(delta_velocity_j);
-    //   }
-    // }
+          let delta_velocity_j = impact_vector.copy();
+          delta_velocity_j.mult((-2 * balls[i].mass * numerator) / denomenator);
+          balls[j].velocity.add(delta_velocity_j);
+        }
+      }
+    }
   }
 }
